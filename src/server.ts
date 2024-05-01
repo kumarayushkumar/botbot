416cf66175f5b179b2ef99f3cacd36f60452e64a
Yaman jain <72979343+yaman-694@users.noreply.github.com>
2024-05-01 19:24:34 +0530

    first commit

---
+++ src/server.ts
@@ -0,0 +1,32 @@
+import { Client, GatewayIntentBits } from "discord.js";
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    // GatewayIntentBits.GuildMembers,
+    // GatewayIntentBits.GuildMessages,
+    // GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user?.tag}!`);
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "ping") {
+    await interaction.reply("Pong!");
+  }
+});
+
+client.on("messageCreate", async (message) => {
+  console.log(message.content);
+});
+
+client.login(process.env.TOKEN);
